7e0b110b35f0ea5fa96e4c5e8d380ec334d4b9cd
Jafsari <justinafsari@yahoo.com>
2018-02-14 15:53:37 -0800

    fixing spec

--- Tests/test/reducers/comments_test.js
+++ Tests/test/reducers/comments_test.js
@@ -4,7 +4,7 @@ import { savecomment } from '../../src/actions'
 
 describe('Comments Reducer', () => {
   it('should handle unknown types', () =>{
-    expect(commentReducer()).to.eql([]);
+    expect(commentReducer(undefined,{})).to.eql([]);
   })
 
   it('handles action of type SAVE_COMMENT', () => {
