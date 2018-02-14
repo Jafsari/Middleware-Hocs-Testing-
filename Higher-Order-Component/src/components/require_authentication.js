14f85faff4607c6e19385df84a42fe4e8a322f38
Jafsari <justinafsari@yahoo.com>
2018-02-13 19:35:10 -0800

    adding context

--- Higher-Order-Component/src/components/require_authentication.js
+++ Higher-Order-Component/src/components/require_authentication.js
@@ -3,7 +3,23 @@ import { connect } from 'react-redux'
 
 export default function(ComposedComponent){
 	class Authentication extends Component {
-		render(){
+		static contextTypes = {
+      router:React.PropTypes.object
+    }
+
+    componentWillMount(){
+      if (!this.props.authenticated){
+        this.context.router.push('/')
+      }
+    }
+
+    componentWillUpdate(nextProps){
+      if(!nextProps.authenticated){
+        this.context.router.push('/')
+      }
+    }
+
+    render(){
 			return <ComposedComponent {...this.props}/>
 		}
 	}
