ce8ce6aafcdc61253a31aa57db1986af853884fc
Jafsari <justinafsari@yahoo.com>
2018-02-14 15:52:04 -0800

    added reducers,actions,container, added tests for reducer,actions

--- Tests/test/components/comment_box_test.js
+++ Tests/test/components/comment_box_test.js
@@ -26,10 +26,11 @@ describe('comment_box', () => {
       component.find('textarea').simulate('change','new comment');
     });
     it('shows that text in the textarea', () => {
-
+      expect(component.find('textarea')).to.have.value('new comment')
     });
     it('when submitted,clears the input', () => {
-
+      component.simulate('submit')
+      expect(component.find('textarea')).to.have.value('')
     });
   });
 });
---
+++ Tests/test/reducers/comments_test.js
@@ -0,0 +1,14 @@
+import { expect } from '../test_helper';
+import { SAVE_COMMENT } from '../../src/actions/types'
+import { savecomment } from '../../src/actions'
+
+describe('Comments Reducer', () => {
+  it('should handle unknown types', () =>{
+    expect(commentReducer()).to.eql([]);
+  })
+
+  it('handles action of type SAVE_COMMENT', () => {
+    const action = { type: SAVE_COMMENT, payload: 'shiba inu' }
+    expect(commentReducer([],action)).to.eql(['shiba inu'])
+  })
+})
