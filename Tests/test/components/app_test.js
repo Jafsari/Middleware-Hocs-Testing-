d2de50fedcbc06f16772fafe7392d6186fd7595d
Jafsari <justinafsari@yahoo.com>
2018-02-14 12:48:01 -0800

    Refactoring tests and adding TDD before implementation

--- Tests/test/components/app_test.js
+++ Tests/test/components/app_test.js
@@ -1,11 +1,15 @@
 import { renderComponent,expect } from '../test_helper';
 import App from '../../src/components/app';
 
+
 describe("App", () => {
-it ('shows the correct Text',() => {
-  
-  const component = renderComponent(App);
-  
-  expect(component).to.contain('Hello World!')
+  let component;
+
+  beforeEach(() => {
+  component = renderComponent(App)
 });
+
+  it('shows a comment box',() =>{
+    expect(component.find('.comment-box')).to.exist;
+  })
 });
--- Tests/test/components/comment_box_test.js
+++ Tests/test/components/comment_box_test.js
@@ -2,19 +2,34 @@ import { renderComponent,expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('comment_box', () => {
+  let component;
+
+  beforeEach(() => {
+   component = renderComponent(CommentBox)
+  });
+
   it('has the correct class', () =>{
-    const component = renderComponent(CommentBox)
     expect(component).to.have.class('comment-box')
-  })
+  });
 
 
-  it('has a text area',() => {
-    const component = renderComponent(CommentBox);
+  it('has a text area',() => {;
     expect(component.find('textarea')).to.exist;
   });
 
-  it('has a button',() => {
-    const component = renderComponent(CommentBox);
+  it('has a button',() => {;
     expect(component.find('button')).to.exist;
+  });
+  describe('entering some text', () => {
+
+    beforeEach(() => {
+      component.find('textarea').simulate('change','new comment');
+    });
+    it('shows that text in the textarea', () => {
+
+    });
+    it('when submitted,clears the input', () => {
+
+    });
   });
 });
